8e07a51c3a437ca798116fcef70afae25b5e0419
mszahan44 <77796789+mszahan44@users.noreply.github.com>
2023-08-17 14:58:37 +0000

    used props for passing the info

--- adopt_me/src/App.js
+++ adopt_me/src/App.js
@@ -1,8 +1,11 @@
-const Pet = () => {
+// using props like self in python for data
+// props for property
+
+const Pet = (props) => {
     return React.createElement("div", {}, [
-        React.createElement("h1", {}, "alex"),
-        React.createElement("h2", {}, "Dog"),
-        React.createElement("h2", {}, "Havanese"),
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
 
     ]);
 };
@@ -14,9 +17,21 @@ const App = () => {
         {},
         [
             React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet),
-            React.createElement(Pet),
-            React.createElement(Pet),
+            React.createElement(Pet, {
+                animal: "Dog",
+                name: "Alex",
+                breed: "Havanese"
+            }),
+            React.createElement(Pet, {
+                animal: "Bird",
+                name: "Pepper",
+                breed: "Doel"
+            }),
+            React.createElement(Pet, {
+                animal: "Cat",
+                name: "Rebec",
+                breed: "Mixed"
+            }),
     ]
     )
 };
