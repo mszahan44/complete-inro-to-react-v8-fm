92db3c58bc8d91ed7a377acada06c6b16fefb042
mszahan44 <77796789+mszahan44@users.noreply.github.com>
2023-08-17 17:50:34 +0000

    initiated npm and installed prettier

--- adopt_me/src/App.js
+++ adopt_me/src/App.js
@@ -2,40 +2,34 @@
 // props for property
 
 const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed),
-
-    ]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
 };
-
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {
-                animal: "Dog",
-                name: "Alex",
-                breed: "Havanese"
-            }),
-            React.createElement(Pet, {
-                animal: "Bird",
-                name: "Pepper",
-                breed: "Doel"
-            }),
-            React.createElement(Pet, {
-                animal: "Cat",
-                name: "Rebec",
-                breed: "Mixed"
-            }),
-    ]
-    )
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Alex",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      animal: "Bird",
+      name: "Pepper",
+      breed: "Doel",
+    }),
+    React.createElement(Pet, {
+      animal: "Cat",
+      name: "Rebec",
+      breed: "Mixed",
+    }),
+  ]);
 };
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(React.createElement(App));
